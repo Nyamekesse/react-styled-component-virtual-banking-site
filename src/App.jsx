8c8c1d8f31e3e8f86a611273a3fe8f9f20771491
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-01-03 14:22:31 +0000

    added and completed NavBar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+};
+
+export default App;
